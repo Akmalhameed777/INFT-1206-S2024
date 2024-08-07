93a3716086202ef0ef4f6213c5d2213b07c58753
AkmalHameed <114934726+AkmalHameed@users.noreply.github.com>
2024-08-06 23:17:24 -0400

    finished part 1

--- Assignment_4/Part1/JavaScript.js
+++
@@ -1,8 +0,0 @@
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
-
-function randomValueFromArray(array){
-    const random = Math.floor(Math.random()*array.length);
-    return array[random];
-  }
